aceac18b5cc8397926c407deaef8086ac4bc84c8
Gian77 <gian.benucci@gmail.com>
2022-01-31 11:33:09 -0500

    new ggplot tricks

--- ggplot2-tricks.R
+++ ggplot2-tricks.R
@@ -1,6 +1,6 @@
 # Examples
 
-# How to deal with legend lables with manual shape and colors
+# How to deal with legend lables with manual shape and colors.
 
 scale_shape_manual("Niche",
                    values = c(19, 2),
@@ -11,3 +11,42 @@ scale_colour_manual(
   values = paletteCB6,
   labels = c("Escanaba", "Hancock", "Lake City", "Lux Arbor", "Rhineland")
 )
+
+
+
+# How to add a color gradient to an object of class *Date* from type
+# "as.Date".
+
+scale_color_gradient(
+  name = NULL,
+  trans = "date",
+  low = "#B2DF8A",
+  high = "#33A02C"
+)
+
+scale_color_gradient(
+  name = NULL,
+  trans = "date",
+  low = "#B2DF8A",
+  high = "#33A02C",
+  guide = guide_colourbar(direction = "vertical")
+)
+
+
+# How to modify axis decimals in an ordination, e.g. form metaMDS.
+
+scale_x_continuous(labels = scales::number_format(accuracy = 0.05))
+scale_y_continuous(labels = scales::number_format(accuracy = 0.05))
+
+
+# How to use *guides* to modify legend appearence.
+
+guides(fill = "none",
+       color = guide_legend(nrow = 2, title="Time"),
+       shape = guide_legend(nrow = 2, title=NULL))
+
+
+
+# How to reduce space between multiple legends.
+
+theme(legend.margin = margin(0,-0.5, 0, 0, unit = "cm"))
